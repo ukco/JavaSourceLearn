e5b8e47b5852b619ef6172a30b80a08092ae67d2
luyinren <18896994109@163.com>
2020-10-13 12:40:42 +0800

    x的平方根

--- src/cn/humblecodeukco/test/leetcode/mySqrt.java
+++ src/cn/humblecodeukco/test/leetcode/mySqrt.java
@@ -4,13 +4,33 @@ package cn.humblecodeukco.test.leetcode;
  * @Author lyr
  * @Date 2020/10/12 21:55
  * @Version 1.0
- * @Description
+ * @Description 69
  */
 public class mySqrt {
 }
 
-class mySqrtSolution {
+// 袖珍计算器算法
+//class mySqrtSolution {
 //    public int mySqrt(int x) {
-//        if (x < 0) return 0;
+//        if (x == 0) return 0;
+//        int ans = (int) Math.exp(0.5 * Math.log(x));
+//        return (long) (ans + 1) * (ans + 1) <= x ? ans + 1 : ans;
 //    }
+//}
+
+// 二分查找
+class mySqrtSolution {
+    public int mySqrt(int x) {
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
 }
