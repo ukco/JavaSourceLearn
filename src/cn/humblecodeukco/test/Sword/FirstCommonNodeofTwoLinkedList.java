ae9cc0775e232ecc433dd6a84299872bad1c7e28
luyinren <18896994109@163.com>
2020-10-23 19:37:34 +0800

    链表的公共节点

--- src/cn/humblecodeukco/test/Sword/FirstCommonNodeofTwoLinkedList.java
+++ src/cn/humblecodeukco/test/Sword/FirstCommonNodeofTwoLinkedList.java
@@ -8,7 +8,7 @@ import cn.humblecodeukco.test.DataStructure.ListNode;
  * @Version 1.0
  * @Description JZ36, 52
  */
-public class FirstCommonNodeofTwoLinkedList {
+/*public class FirstCommonNodeofTwoLinkedList {
     public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
         if (pHead1 == null || pHead2 == null) return null;
         ListNode p1 = pHead1;
@@ -20,6 +20,18 @@ public class FirstCommonNodeofTwoLinkedList {
                 if (p1 == null) p1 = pHead2;
                 if (p2 == null) p2 = pHead1;
             }
+        }
+        return p1;
+    }
+}*/
+
+public class FirstCommonNodeofTwoLinkedList {
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        if (pHead1 == null || pHead2 == null) return null;
+        ListNode p1 = pHead1, p2 = pHead2;
+        while (p1 != p2) {
+            p1 = p1 == null ? pHead2 : p1.next;
+            p2 = p2 == null ? pHead1 : p2.next;
         }
         return p1;
     }
