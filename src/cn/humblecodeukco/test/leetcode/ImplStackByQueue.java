79ec9783dcff30f852eb39e69ac4c7ad3aadcca0
luyinren <18896994109@163.com>
2020-10-28 12:59:18 +0800

    队列实现栈

---
+++ src/cn/humblecodeukco/test/leetcode/ImplStackByQueue.java
@@ -0,0 +1,80 @@
+package cn.humblecodeukco.test.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Author lyr
+ * @Date 2020/10/28 12:39
+ * @Version 1.0
+ * @Description
+ */
+public class ImplStackByQueue {
+    public static void main(String[] args) {
+
+    }
+}
+
+// 一个队列
+class MyStack {
+    Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        int n = queue.size();
+        queue.offer(x);
+        for (int i = 0; i < n; ++i) {
+            queue.offer(queue.poll());
+        }
+    }
+
+    public int pop() {
+        return queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+// 两个队列
+/*
+class MyStack {
+
+    Queue<Integer> queue1;
+    Queue<Integer> queue2;
+
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue2.offer(x);
+        while (!queue1.isEmpty()) {
+            queue2.offer(queue1.poll());
+        }
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+
+    public int pop() {
+        return queue1.poll();
+    }
+
+    public int top() {
+        return queue1.peek();
+    }
+
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}*/
