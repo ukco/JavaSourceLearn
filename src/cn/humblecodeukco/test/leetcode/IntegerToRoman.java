4330788d7c281950df5dec420c76034b4e9149e6
luyinren <18896994109@163.com>
2020-11-12 12:54:58 +0800

    整数转罗马数字

---
+++ src/cn/humblecodeukco/test/leetcode/IntegerToRoman.java
@@ -0,0 +1,47 @@
+package cn.humblecodeukco.test.leetcode;
+
+/**
+ * @Author lyr
+ * @Date 2020/11/12 12:33
+ * @Version 1.0
+ * @Description 12
+ */
+public class IntegerToRoman {
+}
+
+class IntegerToRomanSolution {
+    /**
+     * 贪心
+     * @param num
+     * @return
+     */
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder sb = new StringBuilder();
+        // Loop through each symbol, stopping if num becomes 0.
+        for (int i = 0; i < values.length && num >= 0; ++i) {
+            // Repeat while the current symbol still fits into num.
+            while (values[i] <= num) {
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 硬编码
+     * @param num
+     * @return
+     */
+    public String intToRoman2(int num) {
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        return thousands[num / 1000] + hundreds[num % 1000 / 100] + tens[num % 100 / 10] + ones[num % 10];
+    }
+}
