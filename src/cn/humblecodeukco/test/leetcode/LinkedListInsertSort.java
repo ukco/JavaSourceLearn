14968d297d07c787e5d14b5e3ca9f8de351feebd
luyinren <18896994109@163.com>
2020-11-20 15:24:12 +0800

    对链表进行插入排序

---
+++ src/cn/humblecodeukco/test/leetcode/LinkedListInsertSort.java
@@ -0,0 +1,35 @@
+package cn.humblecodeukco.test.leetcode;
+
+import cn.humblecodeukco.test.DataStructure.ListNode;
+
+/**
+ * @Author lyr
+ * @Date 2020/11/20 14:41
+ * @Version 1.0
+ * @Description 147
+ */
+public class LinkedListInsertSort {
+}
+
+class LinkedListInsertSortSolution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) return head;
+
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head, curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyHead;
+                while (prev.next.val <= curr.val) prev = prev.next;
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+    }
+}
