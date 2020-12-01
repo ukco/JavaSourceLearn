cdf7efe18220864a7dfd1d42cda8c2b8b35ecbc8
luyinren <18896994109@163.com>
2020-12-01 21:26:49 +0800

    算数

--- src/cn/humblecodeukco/Main.java
+++ src/cn/humblecodeukco/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        System.out.println(new Main().Minimumdays(8, new int[] {1,1,1,2,3,5,7,7}));
+        System.out.println(new Main().Answerofjudge(new int[] {6,6,6,6,5,8}));
     }
 
     public int Minimumdays (int n, int[] DEF) {
@@ -24,6 +24,19 @@ public class Main {
             }
         }
         return day;
+    }
+
+    public int Answerofjudge (int[] arr) {
+        // write code here
+        Arrays.sort(arr);
+        double avg = 0;
+        for (int i = 0; i < arr.length; i++) { avg = avg + arr[i] * 1.0 / arr.length; }
+        double mid;
+        if (arr.length % 2 == 0) { mid = (arr[arr.length / 2] + arr[arr.length / 2 - 1]) * 1.0 / 2; }
+        else { mid = arr[arr.length / 2]; }
+        if (mid == avg) { return 0; }
+        else if (mid > avg) { return 1; }
+        else { return -1; }
     }
 }
 
