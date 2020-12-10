2b3582c3a5a7108f9cc483138b111656544bf11c
luyinren <18896994109@163.com>
2020-12-10 21:18:52 +0800

    NC54350

---
+++ src/cn/humblecodeukco/test/niukecompetition/NC54360Solution.java
@@ -0,0 +1,13 @@
+package cn.humblecodeukco.test.niukecompetition;
+
+/**
+ * @Author lyr
+ * @Date 2020/12/10 21:15
+ * @Version 1.0
+ * @Description
+ */
+public class NC54360Solution {
+    public static void main(String[] args) {
+        System.out.println("AC");
+    }
+}
