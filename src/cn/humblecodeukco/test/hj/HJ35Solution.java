df611569d5378230ef46b06f5bc922c2790ec160
luyinren <18896994109@163.com>
2020-12-12 14:38:18 +0800

    HJ35蛇形矩阵[revise]

--- src/cn/humblecodeukco/test/hj/HJ35Solution.java
+++ src/cn/humblecodeukco/test/hj/HJ35Solution.java
@@ -20,7 +20,7 @@ public class HJ35Solution {
             for (int i = 1; i <= num; ++i) {
                 int start = (i - 1) * i / 2 + 1;
                 int step = i + 1;
-                for (int j = 1; j < num - i + 1; ++j) {
+                for (int j = 1; j <= num - i + 1; ++j) {
                     sb.append(start).append(" ");
                     start += step;
                     ++step;
