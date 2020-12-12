1717f48c4dff7e14ff9409eb94e137a96ce9e243
luyinren <18896994109@163.com>
2020-12-12 14:35:39 +0800

    HJ35蛇形矩阵

---
+++ src/cn/humblecodeukco/test/hj/HJ35Solution.java
@@ -0,0 +1,34 @@
+package cn.humblecodeukco.test.hj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @Author lyr
+ * @Date 2020/12/12 14:18
+ * @Version 1.0
+ * @Description HJ35
+ */
+public class HJ35Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        while ((str = br.readLine()) != null) {
+            int num = Integer.parseInt(str);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= num; ++i) {
+                int start = (i - 1) * i / 2 + 1;
+                int step = i + 1;
+                for (int j = 1; j < num - i + 1; ++j) {
+                    sb.append(start).append(" ");
+                    start += step;
+                    ++step;
+                }
+                sb.setCharAt(sb.length() - 1, '\n');
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            System.out.println(sb.toString());
+        }
+    }
+}
