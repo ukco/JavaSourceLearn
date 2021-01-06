b316ad7a1d40c68e9164ca0e1af9bb935e095880
luyinren <18896994109@163.com>
2021-01-06 16:44:50 +0800

    399. 除法求值

---
+++ src/cn/humblecodeukco/test/leetcode/CalcEquation.java
@@ -0,0 +1,203 @@
+package cn.humblecodeukco.test.leetcode;
+
+import java.util.*;
+
+/**
+ * @Author lyr
+ * @Date 2021/1/6 10:30
+ * @Version 1.0
+ * @Description 399. 除法求值
+ */
+public class CalcEquation {
+    /**
+     * 广度优先搜索
+     * 我们可以将整个问题建模成一张图：给定图中的一些点（变量），以及某些边的权值（两个变量的比值），
+     * 试对任意两点（两个变量）求出其路径长（两个变量的比值）。
+     * 因此，我们首先需要遍历 equations 数组，找出其中所有不同的字符串，并通过哈希表将每个不同的字符串
+     * 映射成整数。
+     * 在构建完图之后，对于任何一个查询，就可以从起点出发，通过广度优先搜索的方式，不断更新起点与当前
+     * 点之间的路径长度，直到搜索到终点为止。
+     * @param equations
+     * @param values
+     * @param queries
+     * @return
+     */
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        int nvars = 0;
+        Map<String, Integer> variables = new HashMap<>();
+
+        int n = equations.size();
+        for (int i = 0; i < n; ++i) {
+            if (!variables.containsKey(equations.get(i).get(0))) {
+                variables.put(equations.get(i).get(0), nvars++);
+            }
+            if (!variables.containsKey(equations.get(i).get(1))) {
+                variables.put(equations.get(i).get(1), nvars++);
+            }
+        }
+
+        // 对于每个点，存储其直接连接到的所有点及对应的权值
+        List<Pair>[] edges = new List[nvars];
+        for (int i = 0; i < nvars; ++i) { edges[i] = new ArrayList<>(); }
+        for (int i = 0; i < n; ++i) {
+            int va = variables.get(equations.get(i).get(0)), vb = variables.get(equations.get(i).get(1));
+            edges[va].add(new Pair(vb, values[i]));
+            edges[vb].add(new Pair(va, 1.0 / values[i]));
+        }
+
+        int queriesCount = queries.size();
+        double[] ret = new double[queriesCount];
+        for (int i = 0; i < queriesCount; ++i) {
+            List<String> query = queries.get(i);
+            double result = -1.0;
+            if (variables.containsKey(query.get(0)) && variables.containsKey(query.get(1))) {
+                int ia = variables.get(query.get(0)), ib = variables.get(query.get(1));
+                if (ia == ib) { result = 1.0; }
+                else {
+                    Queue<Integer> points = new LinkedList<>();
+                    points.offer(ia);
+                    double[] ratios = new double[nvars];
+                    Arrays.fill(ratios, -1.0);
+                    ratios[ia] = 1.0;
+
+                    while (!points.isEmpty() && ratios[ib] < 0) {
+                        int x = points.poll();
+                        for (Pair pair : edges[x]) {
+                            int y = pair.index;
+                            double val = pair.value;
+                            if (ratios[y] < 0) {
+                                ratios[y] = ratios[x] * val;
+                                points.offer(y);
+                            }
+                        }
+                    }
+                    result = ratios[ib];
+                }
+            }
+            ret[i] = result;
+        }
+        return ret;
+    }
+
+    /**
+     * Floyd算法
+     * @param equations
+     * @param values
+     * @param queries
+     * @return
+     */
+    public double[] calcEquation2(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        int nvars = 0;
+        Map<String, Integer> variables = new HashMap<>();
+
+        int n = equations.size();
+        for (int i = 0; i < n; ++i) {
+            if (!variables.containsKey(equations.get(i).get(0))) {
+                variables.put(equations.get(i).get(0), nvars++);
+            }
+            if (!variables.containsKey(equations.get(i).get(1))) {
+                variables.put(equations.get(i).get(1), nvars++);
+            }
+        }
+        double[][] graph = new double[nvars][nvars];
+        for (int i = 0; i < nvars; i++) { Arrays.fill(graph[i], -1.0); }
+        for (int i = 0; i < n; i++) {
+            int va = variables.get(equations.get(i).get(0)), vb = variables.get(equations.get(i).get(1));
+            graph[va][vb] = values[i];
+            graph[vb][va] = 1.0 / values[i];
+        }
+
+        for (int k = 0; k < nvars; ++k) {
+            for (int i = 0; i < nvars; ++i) {
+                for (int j = 0; j < nvars; ++j) {
+                    if (graph[i][k] > 0 && graph[k][j] > 0) {
+                        graph[i][j] = graph[i][k] * graph[k][j];
+                    }
+                }
+            }
+        }
+
+        int queriesCount = queries.size();
+        double[] ret = new double[queriesCount];
+        for (int i = 0; i < queriesCount; i++) {
+            List<String> query = queries.get(i);
+            double result = -1.0;
+            if (variables.containsKey(query.get(0)) && variables.containsKey(query.get(1))) {
+                int ia = variables.get(query.get(0)), ib = variables.get(query.get(1));
+                if (graph[ia][ib] > 0) {
+                    result = graph[ia][ib];
+                }
+            }
+            ret[i] = result;
+        }
+        return ret;
+    }
+
+    /**
+     * 带权并查集
+     * @param equations
+     * @param values
+     * @param queries
+     * @return
+     */
+    public double[] calcEquation3(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        int nvars = 0;
+        Map<String, Integer> variables = new HashMap<>();
+        
+        int n = equations.size();
+        for (int i = 0; i < n; i++) {
+            if (!variables.containsKey(equations.get(i).get(0))) {
+                variables.put(equations.get(i).get(0), nvars++);
+            }
+            if (!variables.containsKey(equations.get(i).get(1))) {
+                variables.put(equations.get(i).get(1), nvars++);
+            }
+        }
+        int[] f = new int[nvars];
+        double[] w = new double[nvars];
+        Arrays.fill(w, 1.0);
+        for (int i = 0; i < nvars; i++) { f[i] = i; }
+
+        for (int i = 0; i < n; i++) {
+            int va = variables.get(equations.get(i).get(0)), vb = variables.get(equations.get(i).get(1));
+            merge(f, w, va, vb, values[i]);
+        }
+        int queriesCount = queries.size();
+        double[] ret = new double[queriesCount];
+        for (int i = 0; i < queriesCount; i++) {
+            List<String> query = queries.get(i);
+            double result = -1.0;
+            if (variables.containsKey(query.get(0)) && variables.containsKey(query.get(1))) {
+                int ia = variables.get(query.get(0)), ib = variables.get(query.get(1));
+                int fa = findf(f, w, ia), fb = findf(f, w, ib);
+                if (fa == fb) { result = w[ia] / w[ib]; }
+            }
+            ret[i] = result;
+        }
+        return ret;
+    }
+    private void merge(int[] f, double[] w, int x, int y, double val) {
+        int fx = findf(f, w, x);
+        int fy = findf(f, w, y);
+        f[fx] = fy;
+        w[fx] = val * w[y] / w[x];
+    }
+    private int findf(int[] f, double[] w, int x) {
+        if (f[x] != x) {
+            int father = findf(f, w, f[x]);
+            w[x] = w[x] * w[f[x]];
+            f[x] = father;
+        }
+        return f[x];
+    }
+}
+
+class Pair {
+    int index;
+    double value;
+
+    Pair(int index, double value) {
+        this.index = index;
+        this.value = value;
+    }
+}
