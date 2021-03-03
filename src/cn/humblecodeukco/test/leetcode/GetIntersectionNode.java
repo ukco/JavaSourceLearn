6dc2eb1766a9cf12554b01c5074243664fbe2081
luyinren <18896994109@163.com>
2021-03-03 15:42:19 +0800

    Offer 52. 两个链表的第一个公共节点

---
+++ src/cn/humblecodeukco/test/leetcode/GetIntersectionNode.java
@@ -0,0 +1,20 @@
+package cn.humblecodeukco.test.leetcode;
+
+import cn.humblecodeukco.test.DataStructure.ListNode;
+
+/**
+ * @Author lyr
+ * @Date 2021/3/3 14:38
+ * @Version 1.0
+ * @Description Offer 52. 两个链表的第一个公共节点
+ */
+public class GetIntersectionNode {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode A = headA, B = headB;
+        while (A != B) {
+            A = A != null ? A.next : headB;
+            B = B != null ? B.next : headA;
+        }
+        return A;
+    }
+}
