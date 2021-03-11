d4a99735ddb88dfb09c9c8cec8e32ff535930188
luyinren <18896994109@163.com>
2021-03-11 16:24:39 +0800

    227. 基本计算器II

---
+++ src/cn/humblecodeukco/test/leetcode/CalculateII.java
@@ -0,0 +1,46 @@
+package cn.humblecodeukco.test.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @Author lyr
+ * @Date 2021/3/11 14:05
+ * @Version 1.0
+ * @Description 227. 基本计算器II
+ */
+public class CalculateII {
+    public int calculate(String s) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        char preSign = '+';
+        int num = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = num * 10 + s.charAt(i) - '0';
+            }
+            if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == n - 1) {
+                switch (preSign) {
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(-num);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * num);
+                        break;
+                    default:
+                        stack.push(stack.pop() / num);
+                }
+                preSign = s.charAt(i);
+                num = 0;
+            }
+        }
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+        return ans;
+    }
+}
