c5a368990d5f49c037cbfc4bc1e5bcb59f3222b7
luyinren <18896994109@163.com>
2021-04-27 10:19:49 +0800

    938. 二叉搜索树的范围和

--- src/cn/humblecodeukco/test/leetcode/RangeSumBST.java
+++ src/cn/humblecodeukco/test/leetcode/RangeSumBST.java
@@ -2,6 +2,9 @@ package cn.humblecodeukco.test.leetcode;
 
 import cn.humblecodeukco.test.DataStructure.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @Author lyr
  * @Date 2021/3/18 16:08
@@ -18,8 +21,47 @@ public class RangeSumBST {
     private void dfs(TreeNode root, int low, int high) {
         if (root != null) {
             if (low <= root.val && root.val <= high) { ans += root.val; }
-            else if (low < root.val) { dfs(root.left, low, high); }
-            else if (root.val < high) { dfs(root.right, low, high); }
+            if (low < root.val) { dfs(root.left, low, high); }
+            if (root.val < high) { dfs(root.right, low, high); }
         }
+    }
+
+    /**
+     * 深度优先搜索
+     * @param root
+     * @param low
+     * @param high
+     * @return
+     */
+    public int rangeSumBST2(TreeNode root, int low, int high) {
+        if (root == null) { return 0; }
+        if (root.val > high) { return rangeSumBST2(root.left, low, high); }
+        if (root.val < low) { return rangeSumBST2(root.right, low, high); }
+        return root.val + rangeSumBST2(root.left, low, high) + rangeSumBST2(root.right, low, high);
+    }
+
+    /**
+     * 广度优先搜索
+     * @param root
+     * @param low
+     * @param high
+     * @return
+     */
+    public int rangeSumBST3(TreeNode root, int low, int high) {
+        int sum = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (node == null) { continue; }
+            if (node.val > high) { q.offer(node.left); }
+            else if (node.val < low) { q.offer(node.right); }
+            else {
+                sum += node.val;
+                q.offer(node.left);
+                q.offer(node.right);
+            }
+        }
+        return sum;
     }
 }
